af2c2c5a56f00cad8bb4daeaeffe7611665bcb0d
llee1990 <leonlee123@gmail.com>
2020-03-17 16:59:38 -0700

    Added some code for Order and OrderProcessing classes

--- InventoryManagement.py
+++ InventoryManagement.py
@@ -7,14 +7,42 @@ import pandas
 
 class OrderProcessor:
 
-    def __init__(self):
-        pass
+    factory_map = {
+        "Christmas": None,
+        "Easter": None,
+        "Halloween": None
+    }
+
+    def __init__(self, path):
+        self.path = path
+        self.order_list = []
+
+    def get_orders(self):
+        excel_df = pandas.read_excel(self.path)
+        for row in excel_df.iterrows():
+            self.order_list.append(Order(**row[1]))
 
 
 class Order:
 
-    def __init__(self):
-        pass
+    def __init__(self, order_number, product_id, item, name, **kwargs):
+        self.order_number = order_number
+        self.product_id = product_id
+        self.item_type = item
+        self.name = name
+        self.product_details = {}
+        for arg, value in kwargs.items():
+            if arg != "holiday" and not pandas.isnull(value):
+                self.product_details[arg] = value
+
+    def __str__(self):
+        separator = "-" * 20
+        order_details = f"Order number: {self.order_number}\n" \
+                        f"ID: {self.product_id}\n" \
+                        f"Item type: {self.item_type}\n" \
+                        f"Item name: {self.name}\n" \
+                        f"Product details: {self.product_details}"
+        return '\n'.join([separator, order_details, separator])
 
 
 class Store:
--- UserMenu.py
+++ UserMenu.py
@@ -1,7 +1,21 @@
 """
 
 """
+from InventoryManagement import OrderProcessor, Order
+
+
 class UserMenu:
 
     def __init__(self):
         pass
+
+
+def main():
+    a = OrderProcessor("orders.xlsx")
+    a.get_orders()
+    for order in a.order_list:
+        print(order)
+
+
+if __name__ == "__main__":
+    main()
