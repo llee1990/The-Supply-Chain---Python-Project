7ce62d06f53459ec3ac4bd6aa9eb6736ebbac7ff
llee1990 <leonlee123@gmail.com>
2020-03-17 15:54:14 -0700

    Created basic blueprint classes for InventoryManagement modules

--- InventoryManagement.py
+++ InventoryManagement.py
@@ -1,3 +1,23 @@
 """
 
 """
+
+import pandas
+
+
+class OrderProcessor:
+
+    def __init__(self):
+        pass
+
+
+class Order:
+
+    def __init__(self):
+        pass
+
+
+class Store:
+
+    def __init__(self):
+        pass
