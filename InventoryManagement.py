02e1186ac15e9debe552440f22d2a3077fa78330
JunBeom Han <junbeomh94@gmail.com>
2020-03-21 19:57:33 -0700

    changed factory_map

--- InventoryManagement.py
+++ InventoryManagement.py
@@ -3,23 +3,32 @@
 """
 
 import pandas
-import InventoryFactories
+from InventoryFactories import ChristmasItemFactory, EasterItemFactory,\
+    HalloweenItemFactory
+from InventoryItems import ItemEnum
+from InventoryFactories import FactoryEnum
 
 
 class OrderProcessor:
 
-    factory_map = {"Toy":
-                   {"Christmas": InventoryFactories.SantaShopFactory,
-                    "Easter": InventoryFactories.RobotBunnyFactory,
-                    "Halloween": InventoryFactories.RCSpiderFactory},
-                   "StuffedAnimal":
-                   {"Christmas": InventoryFactories.ReindeerFactory,
-                    "Easter": InventoryFactories.EasterBunnyFactory,
-                    "Halloween": InventoryFactories.DancingSkeletonFactory},
-                   "Candy":
-                   {"Christmas": InventoryFactories.CandyCanesFactory,
-                    "Easter": InventoryFactories.CremeEggsFactory,
-                    "Halloween": InventoryFactories.PCTFactory}}
+    factory_map = {
+        FactoryEnum.EASTER: EasterItemFactory,
+        FactoryEnum.CHRISTMAS: ChristmasItemFactory,
+        FactoryEnum.HALLOWEEN: HalloweenItemFactory
+    }
+
+    # factory_map = {"Toy":
+    #                {"Christmas": InventoryFactories.SantaShopFactory,
+    #                 "Easter": InventoryFactories.RobotBunnyFactory,
+    #                 "Halloween": InventoryFactories.RCSpiderFactory},
+    #                "StuffedAnimal":
+    #                {"Christmas": InventoryFactories.ReindeerFactory,
+    #                 "Easter": InventoryFactories.EasterBunnyFactory,
+    #                 "Halloween": InventoryFactories.DancingSkeletonFactory},
+    #                "Candy":
+    #                {"Christmas": InventoryFactories.CandyCanesFactory,
+    #                 "Easter": InventoryFactories.CremeEggsFactory,
+    #                 "Halloween": InventoryFactories.PCTFactory}}
 
     def __init__(self, path):
         self.path = path
@@ -29,6 +38,7 @@ class OrderProcessor:
         excel_df = pandas.read_excel(self.path)
         for row in excel_df.iterrows():
             self.order_list.append(Order(**row[1]))
+            # yield Order(**row[1])
 
 
 class Order:
@@ -41,7 +51,7 @@ class Order:
         self.name = name
         self.holiday = holiday
         self.product_details = {}
-        self.factory = OrderProcessor.factory_map[item][holiday]
+        self.factory = OrderProcessor.factory_map[FactoryEnum(holiday)]
         for arg, value in kwargs.items():
             if arg != "holiday" and not pandas.isnull(value):
                 self.product_details[arg] = value
@@ -71,9 +81,15 @@ class Store:
             self.inventory[item.name].pop()
 
     def get_item(self, item):
+        print(item)
+        print(self.inventory)
         if item not in self.inventory:
+            new_item = item.factory.create_items(item.item_type, **item)
+            print(new_item)
+
             self.inventory[item.name] = \
                 [new_item for new_item in item.factory().create_items()]
+
         elif len(self.inventory[item]) < int(item.product_details['quantity']):
             for new_item in item.factory().create_items():
                 self.inventory[item.name].append(new_item)
