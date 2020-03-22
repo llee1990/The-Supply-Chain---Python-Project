bd9d0013efa3c19c4e92a14b576e8d2c1da8fdef
JunBeom Han <junbeomh94@gmail.com>
2020-03-21 17:24:06 -0700

    updated InventoryItems.py

--- InventoryItems.py
+++ InventoryItems.py
@@ -5,69 +5,81 @@
 from abc import ABC, abstractmethod
 
 
-class Toys(ABC):
+class Item(ABC):
 
-    def __init__(self):
-        pass
+    def __init__(self, name, description, product_id):
+        self.name = name
+        self.description = description
+        self.product_id = product_id
 
 
-class SantaWorkshop(Toys):
+class Toy(Item):
+
+    def __init__(self, min_age, has_batteries, **kwargs):
+        super().__init__(**kwargs)
+        self.min_age = min_age
+        self.has_batteries = has_batteries
+
+
+class SantaWorkshop(Toy):
 
     def __init__(self, has_batteries, dimensions, num_rooms, **kwargs):
-        pass
-        # if has_batteries.upper() != "N":
-        #     raise AttributeError
-        # if num_rooms < 1:
-        #     raise AttributeError
-        # super().__init__(**kwargs)
-        # self.dimensions = dimensions
-        # self.num_rooms = num_rooms
+        if has_batteries.upper() != "N":
+            raise AttributeError
+        if num_rooms < 1:
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.dimensions = dimensions
+        self.num_rooms = num_rooms
 
 
-class RCSpider(Toys):
+class RCSpider(Toy):
 
     def __init__(self, has_batteries, speed, jump_height, has_glow,
                  spider_type, **kwargs):
-        pass
-        # if has_batteries.upper() != "Y":
-        #     raise AttributeError
-        # if speed < 1:
-        #     raise AttributeError
-        # if jump_height < 1:
-        #     raise AttributeError
-        # if has_glow.upper() != "N" or has_glow.upper() != "Y":
-        #     raise AttributeError
-        # if spider_type.upper() != "Tarantula" or \
-        #         spider_type.upper() != "Wolf Spider":
-        #     raise AttributeError
-        # super().__init__(**kwargs)
-        # self.speed = speed
-        # self.jump_height = jump_height
-        # self.has_glow = has_glow
-        # self.spider_type = spider_type
+        if has_batteries.upper() != "Y":
+            raise AttributeError
+        if speed < 1:
+            raise AttributeError
+        if jump_height < 1:
+            raise AttributeError
+        if has_glow.upper() != "N" or has_glow.upper() != "Y":
+            raise AttributeError
+        if spider_type.upper() != "Tarantula" or \
+                spider_type.upper() != "Wolf Spider":
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.speed = speed
+        self.jump_height = jump_height
+        self.has_glow = has_glow
+        self.spider_type = spider_type
 
 
-class RobotBunny(Toys):
+class RobotBunny(Toy):
 
     def __init__(self, num_sounds, color, **kwargs):
-        pass
-        # if num_sounds < 1:
-        #     raise AttributeError
-        # if color.title() != "Orange" and color.title() != "Blue" and \
-        #         color.title() != "Pink":
-        #     raise AttributeError
-        # super().__init__(**kwargs)
-        # self.num_sounds = num_sounds
-        # self.color = color
+        if num_sounds < 1:
+            raise AttributeError
+        if color.title() != "Orange" and color.title() != "Blue" and \
+                color.title() != "Pink":
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.num_sounds = num_sounds
+        self.color = color
 
 
-class StuffedAnimals(ABC):
+class StuffedAnimal(Item):
 
-    def __init__(self):
-        pass
+    def __init__(self, stuffing, size, fabric, **kwargs):
+        if size.upper() != "S" and size.upper() != "M" and size.upper() != "L":
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.stuffing = stuffing
+        self.size = size
+        self.fabric = fabric
 
 
-class DancingSkeletons(StuffedAnimals):
+class DancingSkeletons(StuffedAnimal):
 
     def __init__(self, stuffing, fabric, has_glow, **kwargs):
         if stuffing.title() != "Polyester Fibrefill":
@@ -78,7 +90,7 @@ class DancingSkeletons(StuffedAnimals):
         self.has_glow = has_glow
 
 
-class Reindeer(StuffedAnimals):
+class Reindeer(StuffedAnimal):
 
     def __init__(self, stuffing, fabric, has_glow, **kwargs):
         if stuffing.title() != "Wool":
@@ -89,51 +101,46 @@ class Reindeer(StuffedAnimals):
         self.has_glow = has_glow
 
 
-class EasterBunny(StuffedAnimals):
+class EasterBunny(StuffedAnimal):
 
     def __init__(self, stuffing, fabric, **kwargs):
-        pass
-        # if stuffing.title() != "Polyester Fibrefill":
-        #     raise AttributeError
-        # if fabric.title() != "Linen":
-        #     raise AttributeError
-        # super().__init__(**kwargs)
+        if stuffing.title() != "Polyester Fibrefill":
+            raise AttributeError
+        if fabric.title() != "Linen":
+            raise AttributeError
+        super().__init__(**kwargs)
 
 
-class Candy(ABC):
+class Candy(Item):
 
     def __init__(self, has_lactose, has_nuts, **kwargs):
-        pass
-        # super().__init__(**kwargs)
-        # self.has_lactose = has_lactose
-        # self.has_nuts = has_nuts
+        super().__init__(**kwargs)
+        self.has_lactose = has_lactose
+        self.has_nuts = has_nuts
 
 
 class PumpkinCaramelToffee(Candy):
 
     def __init__(self, variety, **kwargs):
-        pass
-        # if variety.title() != "Sea Salt" or variety.title() != "Regular":
-        #     raise AttributeError
-        # super().__init__(**kwargs)
-        # self.variety = variety
+        if variety.title() != "Sea Salt" or variety.title() != "Regular":
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.variety = variety
 
 
 class CandyCane(Candy):
 
     def __init__(self, colour, **kwargs):
-        pass
-        # if colour.title() != "Red" or colour.title() != "Green":
-        #     raise AttributeError
-        # super().__init__(**kwargs)
-        # self.colour = colour
+        if colour.title() != "Red" or colour.title() != "Green":
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.colour = colour
 
 
 class CremeEggs(Candy):
 
     def __init__(self, pack_size, **kwargs):
-        pass
-        # if pack_size < 5:
-        #     raise AttributeError
-        # super().__init__(**kwargs)
-        # self.pack_size = pack_size
+        if pack_size < 5:
+            raise AttributeError
+        super().__init__(**kwargs)
+        self.pack_size = pack_size
