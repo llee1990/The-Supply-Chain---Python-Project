86379910e2e6ce78c192650a26acfaed4987003b
llee1990 <leonlee123@gmail.com>
2020-03-21 19:45:38 -0700

    created first draft of UserMenu module, created blueprint for create_report()

--- InventoryManagement.py
+++ InventoryManagement.py
@@ -4,6 +4,8 @@
 
 import pandas
 import InventoryFactories
+from datetime import date
+import time
 
 
 class OrderProcessor:
@@ -29,6 +31,14 @@ class OrderProcessor:
         excel_df = pandas.read_excel(self.path)
         for row in excel_df.iterrows():
             self.order_list.append(Order(**row[1]))
+
+    def order_history(self):
+        history = ""
+        for order in self.order_list:
+            history += f"Order {order.order_number}, Item {order.item_type}, "\
+                f"Product ID {order.product_id}, Name \"{order.name}\", " \
+                f"Quantity {order.product_details['quantity']}\n"
+        return history
 
 
 class Order:
@@ -77,6 +87,24 @@ class Store:
         elif len(self.inventory[item]) < int(item.product_details['quantity']):
             for new_item in item.factory().create_items():
                 self.inventory[item.name].append(new_item)
-                
-    def create_report(self):
-        pass
+
+    @staticmethod
+    def create_report(orders):
+        local_time = time.localtime()
+        day = date.today().day
+        if day < 10:
+            day = '0' + str(day)
+        month = date.today().month
+        if month < 10:
+            month = '0' + str(month)
+        year = str(date.today().year)[0:2]
+        hour = time.strftime('%H', local_time)
+        minute = time.strftime('%M', local_time)
+        file_name = f"DTR_{day}{month}{year}_{hour}{minute}"
+        with open(f"{file_name}.txt", mode='w', encoding='utf-8') as file:
+            title = 'HOLIDAY STORE - DAILY TRANSACTION REPORT(DRT)\n'
+            date_time = f"{day}-{month}-{year} {hour}:{minute}\n"
+            orders = orders.order_history()
+            data = title + date_time + orders
+            file.write(data)
+
