b69ff3bfcf6ef8800f9bda67352541d5a5af376f
JunBeom Han <junbeomh94@gmail.com>
2020-03-25 03:03:51 -0700

    added gui and load_intro()

--- UserMenu.py
+++ UserMenu.py
@@ -8,11 +8,35 @@ import texttable
 
 class UserMenu:
 
+    BORDER_LENGTH = 35
+
     def __init__(self):
         self.order_processor = OrderProcessor()
         self.store = Store()
+        UserMenu.load_intro()
+
+    @staticmethod
+    def load_intro():
+        """
+        generated from http://patorjk.com/
+        """
+        str = "\n" \
+              "       🎃 🎃 🎃    🐰    🐰    🎅🎅🎅🎅 \n" \
+              "          🎃       🐰    🐰    🎅     \n" \
+              "          🎃       🐰🐰🐰🐰    🎅🎅🎅 \n" \
+              "          🎃       🐰    🐰    🎅    \n" \
+              "          🎃       🐰    🐰    🎅🎅🎅🎅\n" \
+              "\n\n" \
+              "   🐰🐰🐰🐰    🎅    🎅      🎁🎁🎁     🎃🎃🎃🎃 \n" \
+              "   🐰          🎅    🎅     🎁    🎁    🎃    🎃 \n" \
+              "    🐰🐰🐰      🎅 🎅 🎅    🎁     🎁    🎃🎃🎃 \n" \
+              "         🐰     🎅    🎅    🎁    🎁     🎃      \n" \
+              "   🐰🐰🐰🐰      🎅    🎅     🎁🎁🎁      🎃       \n"
+        print(str)
 
     def process_web_orders(self):
+        print("\n\n1. Process web orders")
+        print("-" * self.BORDER_LENGTH)
         try:
             orders = self.order_processor.get_orders()
             order_count = 0
@@ -20,11 +44,18 @@ class UserMenu:
                 self.store.process_item(order)
                 order_count += 1
         except FileNotFoundError as fne:
-            print("Error: " + str(fne))
+            print("Error: " + str(fne).split("]")[1].strip())
         else:
-            print(f"\nSuccessfully processed {order_count} orders...\n")
+            print(f"\nSuccessfully processed {order_count} orders...")
+        print("-" * self.BORDER_LENGTH + "\n\n")
 
     def check_inventory(self):
+        print("\n\n2. CheckInventory")
+        print("-" * self.BORDER_LENGTH)
+        if not self.store.inventory:
+            print("No items in inventory...")
+            print("-" * self.BORDER_LENGTH + "\n\n")
+            return
         table = texttable.Texttable()
         table.set_deco(table.HEADER)
         table.add_rows([["Item Name",    "Stock", "Quantity"]])
@@ -46,20 +77,20 @@ class UserMenu:
             row.append(len(value))
             table.add_row(row)
         print(f"\n{table.draw()}\n")
+        print("-" * self.BORDER_LENGTH + "\n\n")
 
     def exit_program(self):
-        print("Printing report...")
+        print("\n\n3. Exit Program")
+        print("-" * self.BORDER_LENGTH)
+        print("Printing report...\n")
         self.store.create_report()
         time.sleep(0.5)
         print("Exiting program...")
         time.sleep(0.5)
+        print("-" * self.BORDER_LENGTH + "\n\n")
         exit()
 
-    @staticmethod
-    def load_intro():
-        pass
-
-    def execute(self):
+    def execute_program(self):
         menu_options = {
             1: self.process_web_orders,
             2: self.check_inventory,
@@ -67,13 +98,13 @@ class UserMenu:
         }
         program_running = True
         while program_running:
-            print("-" * 15)
+            print("-" * self.BORDER_LENGTH)
             print("What would you like to do today?")
-            print("-" * 15)
+            print("-" * self.BORDER_LENGTH)
             print("1. Process web orders")
             print("2. Check inventory")
             print("3. Exit program")
-            print("-" * 15)
+            print("-" * self.BORDER_LENGTH)
             user_input = 0
             while user_input < 1 or user_input > 3:
                 try:
@@ -87,7 +118,7 @@ class UserMenu:
 
 def main():
     program = UserMenu()
-    program.execute()
+    program.execute_program()
 
 
 if __name__ == "__main__":
