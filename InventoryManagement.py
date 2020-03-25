45b4714af06f3902585911bfa3d17382c3f24789
llee1990 <leonlee123@gmail.com>
2020-03-24 20:00:58 -0700

    changed appropriate variables and methods in InventoryManagement to private

--- InventoryManagement.py
+++ InventoryManagement.py
@@ -18,17 +18,10 @@ class OrderProcessor:
     }
 
     def __init__(self, path):
-        self.path = path
-        self.order_list = []
-
-    def add_orders(self, order):
-        self.order_list.append(order)
-
-    def clear_order_list(self):
-        self.order_list = []
+        self._path = path
 
     def get_orders(self):
-        excel_df = pandas.read_excel(self.path)
+        excel_df = pandas.read_excel(self._path)
         for row in excel_df.iterrows():
             yield Order(**row[1])
 
@@ -37,35 +30,35 @@ class Order:
 
     def __init__(self, order_number, product_id, item, name, holiday,
                  **kwargs):
-        self.order_number = order_number
-        self.product_id = product_id
-        self.item_type = item
-        self.name = name
-        self.holiday = holiday
-        self.factory = OrderProcessor.factory_map[FactoryEnum(holiday)]()
+        self._order_number = order_number
+        self._product_id = product_id
+        self._item_type = item
+        self._name = name
+        self._holiday = holiday
+        self._factory = OrderProcessor.factory_map[FactoryEnum(holiday)]()
         # Product details to instantiate Item objects
-        self.product_details = {'name': name, 'product_id': product_id,
+        self._product_details = {'name': name, 'product_id': product_id,
                                 'order_number': order_number}
         for arg, value in kwargs.items():
             if arg != "holiday" and not pandas.isnull(value):
-                self.product_details[arg] = value
+                self._product_details[arg] = value
 
     def get_order_history(self):
         history = ""
-        history += f"Order {self.order_number}, Item {self.item_type}, "\
-                f"Product ID {self.product_id}, Name \"{self.name}\", " \
-                f"Quantity {self.product_details['quantity']}\n"
+        history += f"Order {self._order_number}, Item {self._item_type}, "\
+                   f"Product ID {self._product_id}, Name \"{self._name}\", " \
+                   f"Quantity {self._product_details['quantity']}\n"
         return history
 
     def __str__(self):
         separator = "-" * 20
-        order_details = f"Order number: {self.order_number}\n" \
-                        f"ID: {self.product_id}\n" \
-                        f"Item type: {self.item_type}\n" \
-                        f"Item name: {self.name}\n\n" \
+        order_details = f"Order number: {self._order_number}\n" \
+                        f"ID: {self._product_id}\n" \
+                        f"Item type: {self._item_type}\n" \
+                        f"Item name: {self._name}\n\n" \
                         f">>Product details<<"
         product_details = ""
-        for key, value in self.product_details.items():
+        for key, value in self._product_details.items():
             product_details += f"{key}: {value}\n"
         return '\n'.join([separator, order_details,
                           product_details, separator])
@@ -80,20 +73,20 @@ class Store:
         self.order_history = []
 
     def receive_order(self, order):
-        self.process_item(order)
+        self.__process_item(order)
 
     def update_inventory_item(self, order, quantity):
         while quantity != 0:
             self.inventory[order.name].pop()
             quantity -= 1
 
-    def process_item(self, order):
+    def __process_item(self, order):
         factory = order.factory
         order_name = order.name
         try:
             new_item = factory.create_items(item_type=order.item_type,
                                             **order.product_details)
-            self.append_order_history(order, new_item)
+            self.__append_order_history(order, new_item)
             order_amount = int(order.product_details['quantity'])
 
             # item does not exist in inventory
@@ -114,12 +107,12 @@ class Store:
         except InvalidDataError:
             new_item = factory.create_items(item_type=order.item_type,
                                             **order.product_details)
-            self.append_order_history(order, new_item)
+            self.__append_order_history(order, new_item)
             pass
 
         # TODO: Add method to update order_history
 
-    def append_order_history(self, order, item):
+    def __append_order_history(self, order, item):
         if item.error_message == "":
             self.order_history.append(order.get_order_history())
         else:
@@ -151,15 +144,3 @@ class Store:
             orders = "\n".join(self.order_history)
             data = title + date_time + orders
             file.write(data)
-
-
-def main():
-
-    a = OrderProcessor("orders.xlsx")
-    c = Store()
-    for order in a.get_orders():
-        c.receive_order(order)
-
-
-if __name__ == "__main__":
-    main()
