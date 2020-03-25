e95e30df46161cfd19dd5d0f6e0d3279e6f61669
JunBeom Han <junbeomh94@gmail.com>
2020-03-25 02:03:15 -0700

    added default message

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -5,6 +5,8 @@
 
 class InvalidDataError(Exception):
 
+    DEFAULT_MESSAGE = "Could not process order data was corrupted, "
+
     def __init__(self, message=""):
         self.message = message
-        super().__init__(message)
+        super().__init__(f"{self.DEFAULT_MESSAGE}, " + message)
