007a7cc345f70b88a08079afec06dd4abfa64605
llee1990 <leonlee123@gmail.com>
2020-03-25 11:34:18 -0700

    added docstrings for InvalidDataError

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -4,9 +4,11 @@
 
 
 class InvalidDataError(Exception):
+    """Custom exception for invalid data entries"""
 
     DEFAULT_MESSAGE = "Could not process order data was corrupted"
 
     def __init__(self, message=""):
+        """Initializes an InvalidDataError object"""
         self.message = message
         super().__init__(f"{self.DEFAULT_MESSAGE}, " + message)
